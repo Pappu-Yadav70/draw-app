6d7ba981700d7888968dc61d9c46e96655b7d0d3
pappu-yadav70 <technoyadav1234@gmail.com>
2025-05-15 23:31:35 +0530

    add api  endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,38 @@
 import express from "express";
+import  Jwt  from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3000)
+
+
+app.post('/signup',async(req,res)=>{
+    //db call
+    res.json({
+        userId : "123"
+    })
+
+    
+})
+
+
+app.get('/signin',async(req,res)=>{
+    const userId = 1;
+    const token =  Jwt.sign({
+        userId
+    },JWT_SECRET)
+
+    res.json({token})
+})
+
+app.post('/room',middleware,(req,res)=>{
+    // db call
+
+    res.json({
+        roomId:123
+    })
+
+})
+
+app.listen(3001)
